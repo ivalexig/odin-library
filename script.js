4d5c5dbbbd93fc20263e4dc4e9b919e5ea174616
Alex Ivanov <ivalexig@gmail.com>
2023-08-07 21:25:11 +1200

    Book object and simple add function created

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let myLibrary = [];
 
-function Book() {
-  // todo
+function Book(title, author, pagesNum, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pagesNum = pagesNum;
+  this.isRead = isRead;
+
+  this.info = function () {
+    return `${title} by ${author}, ${pagesNum} pages, ${
+      isRead ? "read" : "not read yet"
+    }`;
+  };
 }
 
-function addBookToLibrary() {
-  // todo
+function addBookToLibrary(title, author, pagesNum, isRead) {
+  myLibrary.push(new Book(title, author, pagesNum, isRead));
 }
